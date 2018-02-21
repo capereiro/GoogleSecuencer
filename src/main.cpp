d8889f9eb6d4c237a96de20f51456f295dd524ce
Carlos Pereiro <carlospereiro@Carloss-MacBook-Pro.local>
2018-02-21 01:37:45 -0300

    Blink example

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,11 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(800);
+  delay(3000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
 
    // wait for a second
-  delay(1000);
+  delay(300);
 }
